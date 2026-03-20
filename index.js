7ccdaa9a4790b4b4d2ed28d5eccf1e9d6b94880d
CANELA LEDEZMA <49193970@ort>
2026-03-20 11:39:31 -0300

    EJ01 terminado

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 import fs from 'fs';
+
+fs.readFile('productos.json', 'utf-8', (err, data) => {
+  if (err) {
+    console.log('El archivo no se pudo leer:', err.message);
+    return;
+  }
+
+  const productos = JSON.parse(data);
+
+  console.log('Productos:');
+  productos.forEach((producto) => 
+    {
+    console.log(`${producto.nombre} - $${producto.precio}`);
+    });
+});
