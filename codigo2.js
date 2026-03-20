e7f25e7f65d8b74d7afbb01782180d6556c99349
FELIPE MANUEL ANDRACA <48803376@ort>
2026-03-20 11:45:56 -0300

    ej05 terminado

---
+++ codigo2.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+//--------EJ05-----------------//
+
+const productos = JSON.parse(fs.readFileSync('productos.json','utf8'));
+
+const buscarProducto = producto => {
+    const productoBuscado = productos.find(e => e.nombre === producto);
+    if(productoBuscado != false)
+    {
+        console.log(`Producto encontrado`);
+        console.log(`Nombre: ${productoBuscado.nombre}`);
+        console.log(`Precio: ${productoBuscado.precio}`);      
+    }
+    else
+    {
+        console.log(`Producto no encontrado`);
+    }
+}
+buscarProducto("Mouse");
+
+//--------EJ06-----------------//
+
